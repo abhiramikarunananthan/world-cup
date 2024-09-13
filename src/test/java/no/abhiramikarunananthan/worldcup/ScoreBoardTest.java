51b9e5feac74dcc947a4a2ac1fa1b5a1a5ae66ff
abhi <abhiramk@stud.ntnu.no>
2024-09-13 20:30:38 +0200

    implemented startGameTest according to TDD

--- src/test/java/no/abhiramikarunananthan/worldcup/ScoreBoardTest.java
+++ src/test/java/no/abhiramikarunananthan/worldcup/ScoreBoardTest.java
@@ -1,5 +1,42 @@
 package no.abhiramikarunananthan.worldcup;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 class ScoreBoardTest {
+
+    private ScoreBoard scoreBoard;
+    private Game gameOne;
+    private Game gameTwo;
+
+
+@BeforeEach
+//arrange
+    public void setUp(){
+    scoreBoard = new ScoreBoard();
+    gameOne = new Game("Canada", "USA");
+    gameTwo = new Game("Spain", "Norway");
+}
+
+@Test
+    void startGameTest() {
+
+    //act
+    boolean firstGameStarted = scoreBoard.startGame(gameOne);
+    boolean secondGameStarted = scoreBoard.startGame(gameTwo);
+    boolean sameAsFirstGameStarted = scoreBoard.startGame(gameOne);
+
+
+    //assert
+    assertTrue(firstGameStarted);
+    assertTrue(secondGameStarted);
+    assertFalse(sameAsFirstGameStarted);
+
+}
+
+
 
 }
