167f4ddc36fa16fcc8c189d91d8d9f394c3cab78
abhi <abhiramk@stud.ntnu.no>
2024-09-13 20:04:56 +0200

    added variables and constructor to ScoreBoard class

--- src/main/java/no/abhiramikarunananthan/worldcup/ScoreBoard.java
+++ src/main/java/no/abhiramikarunananthan/worldcup/ScoreBoard.java
@@ -1,4 +1,19 @@
 package no.abhiramikarunananthan.worldcup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScoreBoard {
+
+    private List<Game> scoreBoard;
+
+    private List<Game> summary;
+
+
+    public ScoreBoard() {
+        this.scoreBoard = new ArrayList<Game>();
+        this.summary = new ArrayList<Game>();
+    }
+
+
 }
