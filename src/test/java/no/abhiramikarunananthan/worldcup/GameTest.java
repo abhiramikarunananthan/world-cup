fbf4c84e042e2aa40caf44afd814cacbdf19e809
abhi <abhiramk@stud.ntnu.no>
2024-09-13 18:53:57 +0200

    implemented getHomeTeam test according to TDD

--- src/test/java/no/abhiramikarunananthan/worldcup/GameTest.java
+++ src/test/java/no/abhiramikarunananthan/worldcup/GameTest.java
@@ -1,7 +1,35 @@
 package no.abhiramikarunananthan.worldcup;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    private Game gameOne;
+    private Game gameTwo;
+    private Game gameThree;
+    private Game gameFour;
+
+
+    @BeforeEach
+    //arrange
+    public void setUp(){
+        gameOne = new Game("Canada", "USA", 1);
+        gameTwo = new Game("Spain", "Norway", 2);
+        gameThree = new Game("Sweden", "England", 3);
+        gameFour = new Game("Croatia", "Denmark", 4);
+    }
+
+    @Test
+    void getHomeTeam(){
+        //act
+        String homeTeamOne = gameOne.getHomeTeam();
+        String homeTeamThree = gameThree.getHomeTeam();
+
+        //assert
+        assertEquals(homeTeamOne, "canada");
+        assertEquals(homeTeamThree, "sweden");
+    }
 }
