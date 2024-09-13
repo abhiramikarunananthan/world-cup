7aaa844ac77df832b66c50c25f5fde9e51455c74
abhi <abhiramk@stud.ntnu.no>
2024-09-13 19:51:11 +0200

    added javadoc to Game class

--- src/main/java/no/abhiramikarunananthan/worldcup/Game.java
+++ src/main/java/no/abhiramikarunananthan/worldcup/Game.java
@@ -1,5 +1,8 @@
 package no.abhiramikarunananthan.worldcup;
 
+/**
+ * Game class
+ */
 public class Game {
 
     private String homeTeam;
@@ -9,7 +12,11 @@ public class Game {
     private int id;
     private static int nextId = 1;
 
-
+    /**
+     * constructor for game class
+     * @param homeTeam home team of the game
+     * @param awayTeam away team of the game
+     */
     public Game(String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam.toLowerCase();
         this.awayTeam = awayTeam.toLowerCase();
@@ -18,29 +25,56 @@ public class Game {
         this.id = nextId++;
     }
 
+    /**
+     * get method to retrieve home team of the game
+     */
     public String getHomeTeam() {
         return homeTeam;
     }
 
+    /**
+     * get method to retrieve away team of the game
+     */
     public String getAwayTeam() {
         return awayTeam;
     }
 
+    /**
+     * get method to retrieve unique id of game
+     * @return unique game id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * get method to retrieve score of the home team
+     * @return home team's score
+     */
     public int getHomeScore() {
         return homeScore;
     }
+
+    /**
+     * get method to retrieve score of the away team
+     * @return away team's score
+     */
     public int getAwayScore() {
         return awayScore;
     }
 
+    /**
+     * set method to set score of the home team
+     * @param homeScore home team's new score
+     */
     public void setHomeScore(int homeScore) {
         this.homeScore = homeScore;
     }
 
+    /**
+     * set method to set score of the away team
+     * @param awayScore away team's new score
+     */
     public void setAwayScore(int awayScore) {
         this.awayScore = awayScore;
     }
