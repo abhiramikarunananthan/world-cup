5e343eba209316a8418fb619481725a71f95cf58
abhi <abhiramk@stud.ntnu.no>
2024-09-13 18:45:24 +0200

    implemented variables and constructor to Game class

--- src/main/java/no/abhiramikarunananthan/worldcup/Game.java
+++ src/main/java/no/abhiramikarunananthan/worldcup/Game.java
@@ -2,4 +2,17 @@ package no.abhiramikarunananthan.worldcup;
 
 public class Game {
 
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private int id;
+
+    public Game(String homeTeam, String awayTeam, int id) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.id = id;
+    }
 }
