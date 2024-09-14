346551488d37185dec4ce5f003f0bf7c68b1b918
abhi <abhiramk@stud.ntnu.no>
2024-09-14 13:12:09 +0200

    modified finishGameTest according to TDD and made small changes on other tests

--- src/test/java/no/abhiramikarunananthan/worldcup/ScoreBoardTest.java
+++ src/test/java/no/abhiramikarunananthan/worldcup/ScoreBoardTest.java
@@ -3,6 +3,7 @@ package no.abhiramikarunananthan.worldcup;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -11,77 +12,87 @@ class ScoreBoardTest {
     private ScoreBoard scoreBoard;
     private Game gameOne;
     private Game gameTwo;
+    private Game gameThree;
 
 
-@BeforeEach
+    @BeforeEach
 //arrange
-    public void setUp(){
-    scoreBoard = new ScoreBoard();
-    gameOne = new Game("Canada", "USA");
-    gameTwo = new Game("Spain", "Norway");
-}
+    public void setUp() {
+        scoreBoard = new ScoreBoard();
+        gameOne = new Game("Canada", "USA");
+        gameTwo = new Game("Spain", "Norway");
+        gameThree = new Game("Sweden", "Denmark");
 
-@Test
+    }
+
+    @Test
     void startGameTest() {
 
-    //act
-    boolean firstGameStarted = scoreBoard.startGame(gameOne);
-    boolean secondGameStarted = scoreBoard.startGame(gameTwo);
-    boolean sameAsFirstGameStarted = scoreBoard.startGame(gameOne);
+        //act
+        boolean firstGameStarted = scoreBoard.startGame(gameOne);
+        boolean sameAsFirstGameStarted = scoreBoard.startGame(gameOne);
 
 
-    //assert
-    assertTrue(firstGameStarted);
-    assertTrue(secondGameStarted);
-    assertFalse(sameAsFirstGameStarted);
+        //assert
+        assertTrue(firstGameStarted, "Game should be started for valid input.");
+        assertFalse(sameAsFirstGameStarted, "Game should not start as this exact game has already started.");
 
-}
+    }
 
-@Test
-    void updateScoreTest(){
+    @Test
+    void updateScoreTest() {
 
-    //arrange
-    scoreBoard.startGame(gameOne);
-    String homeTeam = gameOne.getHomeTeam();
-    String awayTeam = gameOne.getAwayTeam();
-    String testForCapitalLetterHomeTeamName = "CANADA";
-    int newScore = 3;
-    int illegalScore = -4;
+        //arrange
+        scoreBoard.startGame(gameOne);
+        String homeTeam = gameOne.getHomeTeam();
+        String awayTeam = gameOne.getAwayTeam();
+        String testForCapitalLetterHomeTeamName = gameOne.getHomeTeam().toUpperCase();
+        int newScore = 3;
+        int illegalScore = -4;
 
-    //act
-    boolean trueScore = scoreBoard.updateScore(homeTeam, awayTeam, newScore, newScore);
-    boolean trueTeamName = scoreBoard.updateScore(testForCapitalLetterHomeTeamName, awayTeam, newScore, newScore);
+        //act
+        boolean trueScore = scoreBoard.updateScore(homeTeam, awayTeam, newScore, newScore);
+        boolean trueTeamName = scoreBoard.updateScore(testForCapitalLetterHomeTeamName, awayTeam, newScore, newScore);
 
 
-    //assert
-    assertTrue(trueScore, "Score should be updated successfully for valid input.");
-    assertTrue(trueTeamName, "Score should be updated successfully for valid input.");
+        //assert
+        assertTrue(trueScore, "Score should be updated successfully for valid input.");
+        assertTrue(trueTeamName, "Score should be updated successfully for valid input.");
 
-    //act & assert
-    assertThrows(IllegalArgumentException.class, () -> {
-        scoreBoard.updateScore(homeTeam, awayTeam, newScore, illegalScore);
-    }, "Updating score with a negative value should throw an IllegalArgumentException.");
+        //act & assert
+        assertThrows(IllegalArgumentException.class, () -> {
+            scoreBoard.updateScore(homeTeam, awayTeam, newScore, illegalScore);
+        }, "Updating score with a negative value should throw an IllegalArgumentException.");
 
-}
+    }
 
-@Test
-    void finishGameTest(){
+    @Test
+    void finishGameTest() {
 
-    String existingHomeTeam = gameOne.getHomeTeam();
-    String existingAwayTeam = gameOne.getAwayTeam();
-    String nonExistingHomeTeam = "japan";
-    String checkCapitalLettersHomeTeam = "CANADA";
+        //arrange
+        scoreBoard.startGame(gameOne);
+        scoreBoard.startGame(gameTwo);
+        scoreBoard.startGame(gameThree);
 
-    //act
-    boolean gameFinishedTrue = scoreBoard.finishGame(existingHomeTeam, existingAwayTeam);
-    boolean gameNotFinishedNonExistingTeam = scoreBoard.finishGame(nonExistingHomeTeam, existingAwayTeam);
-    boolean checkCapitalLettersGameFinishedTrue = scoreBoard.finishGame(checkCapitalLettersHomeTeam, existingAwayTeam);
+        String existingHomeTeamGameOne = gameOne.getHomeTeam();
+        String existingAwayTeamGameOne = gameOne.getAwayTeam();
+        String existingAwayTeamGameTwo = gameTwo.getAwayTeam();
+        String checkCapitalLettersHomeTeam = gameThree.getHomeTeam().toUpperCase();
+        String existingAwayTeamGameThree = gameThree.getAwayTeam();
+        String nonExistingHomeTeam = "japan";
 
-    assertTrue(gameFinishedTrue);
-    assertFalse(gameNotFinishedNonExistingTeam);
-    assertTrue(checkCapitalLettersGameFinishedTrue);
 
-}
+        //act
+        boolean gameFinishedTrue = scoreBoard.finishGame(existingHomeTeamGameOne, existingAwayTeamGameOne);
+        boolean gameNotFinishedNonExistingTeam = scoreBoard.finishGame(nonExistingHomeTeam, existingAwayTeamGameTwo);
+        boolean checkCapitalLettersGameFinishedTrue = scoreBoard.finishGame(checkCapitalLettersHomeTeam, existingAwayTeamGameThree);
+
+        assertTrue(gameFinishedTrue);
+        assertFalse(gameNotFinishedNonExistingTeam);
+        assertTrue(checkCapitalLettersGameFinishedTrue);
+
+    }
+
 
 
 
